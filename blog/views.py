af2f54efd18c71cca458f57cf359ae6f7749f0c6
DawidSzoka1 <oficer130@gmail.com>
2023-07-17 16:29:57 +0200

    calendar

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-from django.contrib.auth.mixins import PermissionRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, UserPassesTestMixin, LoginRequiredMixin
 from .models import Post
+from parent.models import ParentA
 from director.models import Kid
+from django.contrib.auth.models import Permission
+import calendar
+from django.utils import timezone
+from calendar import HTMLCalendar
 from django.views.generic import (
     ListView,
     DetailView,
@@ -12,12 +17,20 @@ from django.views.generic import (
 )
 
 
-class CalendarKid(View):
+class CalendarKid(LoginRequiredMixin, View):
     def get(self, request, pk):
-        kid = Kid.objects.get(id=pk)
-        start = Kid.start
-        return render(request, 'calendar.html', {'kid': kid})
+        permissions = Permission.objects.filter(user=request.user.id)
+        month_number = int(timezone.now().month)
+        year = int(timezone.now().year)
+        cal = HTMLCalendar().formatyear(year, month_number)
 
+        if permissions[0].name == 'director permission':
+            kid = Kid.objects.get(id=pk)
+            return render(request, 'calendar.html', {'kid': kid})
+        elif permissions[0].name == 'parent permission':
+            parent = ParentA.objects.get(user=request.user.id)
+            kids = parent.kid_set.all()
+            return render(request, 'calendar.html', {'kids': kids, 'cal': cal})
 
 class Home(View):
     def get(self, request):
