ecc50d141f7629a9cf8c12dd1274feee03e84c41
DawidSzoka1 <oficer130@gmail.com>
2023-08-08 19:08:42 +0200

    contact_model creat when profile is created and saved

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from director.models import User, Director
+from director.models import User, Director, ContactModel
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
@@ -11,10 +11,11 @@ def create_profile(sender, instance, created, **kwargs):
         content_type = ContentType.objects.get_for_model(Director)
         permission = Permission.objects.get(content_type=content_type, codename='is_director')
         dir_user = Director.objects.create(user=instance)
+        contact_model = ContactModel.objects.create(director=dir_user)
         dir_user.user.user_permissions.add(permission)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.director.save()
-
+    instance.contactmodel.save()
