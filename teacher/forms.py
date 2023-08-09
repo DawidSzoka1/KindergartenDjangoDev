a103b1a35f43df9862649e6c2f992d8e81377a03
DawidSzoka1 <oficer130@gmail.com>
2023-08-09 18:10:04 +0200

    teacher update form

--- teacher/forms.py
+++ teacher/forms.py
@@ -3,14 +3,10 @@ from .models import Employee
 from director.models import Director
 
 
-# class TeacherUpdateForm(forms.ModelForm):
-#
-#     def __init__(self, *args, current_user=None, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         if current_user is not None:
-#             self.fields['group'].queryset = Director.objects.get(user=current_user.id).groups_set.all()
-#
-#     class Meta:
-#         model = Teacher
-#         fields = ['role', '', 'salary']
-#         widgets = {'group': forms.Select}
+class TeacherUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+
