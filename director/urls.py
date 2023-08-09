09da6e37822b086a0bffeacdb0435e2c9484952d
DawidSzoka1 <oficer130@gmail.com>
2023-08-09 18:09:56 +0200

    urls

--- director/urls.py
+++ director/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("director/profile/", views.DirectorProfileView.as_view(), name="director_profile"),
     path("contact/", views.ContactView.as_view(), name="contact"),
     path("contact/update/", views.ContactUpdateView.as_view(), name="contact-update"),
+
 ]
--- teacher/urls.py
+++ teacher/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('director/teacher/details/<int:pk>/', views.TeacherDetailsView.as_view(), name='teacher_details'),
     path('director/teacher/update/<int:pk>/', views.TeacherUpdateView.as_view(), name='teacher_update'),
     path('director/teacher/search/', views.TeacherSearchView.as_view(), name='teacher-search'),
+    path('employee/profile/', views.EmployeeProfileView.as_view(), name='teacher-profile'),
 ]
