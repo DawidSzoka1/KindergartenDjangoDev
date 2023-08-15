1f2cd86981e0e35d4b260192feb17e95aa4e1579
DawidSzoka1 <oficer130@gmail.com>
2023-08-15 19:30:26 +0200

    js

--- blog/static/calendar.js
+++ blog/static/calendar.js
@@ -10,18 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
             const day = this.firstChild.innerHTML
             const monthYear = this.parentElement.parentElement.children[0].children[0].innerHTML
 
-            // console.log(dropdown.children[1].children[0].children[0].children[0].children[0].value)
-            if (parseInt(day) >= currentDay.value) {
-                dropdown.style.display = 'block'
-                if (user.value === "{'parent.is_parent'}"){
+            if (user.value === "{'parent.is_parent'}") {
+                if (parseInt(day) > parseInt(currentDay.value)) {
+                    dropdown.style.display = 'block'
                     console.log(dropdown.children[1].children[0].children[0].children[0].value = `${day} ${monthYear} 2`)
-
-                }else if (user.value === "{'teacher.is_teacher'}"){
+                }
+            } else if (parseInt(day) >= currentDay.value) {
+                dropdown.style.display = 'block'
+                if (user.value === "{'teacher.is_teacher'}") {
                     // do zrobienia
                     console.log(user.value)
                     console.log(dropdown.children[1].children[0].children[0].children[0].value = `${day} ${monthYear} 2`)
 
-                }else{
+                } else {
                     dropdown.children[1].children[0].children[0].children[0].children[0].value = `${day} ${monthYear} 1`;
                     dropdown.children[1].children[1].children[0].children[0].children[0].value = `${day} ${monthYear} 0`;
                     dropdown.children[1].children[2].children[0].children[0].children[0].value = `${day} ${monthYear} 2`;
