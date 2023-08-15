ddcb86c901e47d01715de89528167e07f1ed2acf
DawidSzoka1 <oficer130@gmail.com>
2023-08-15 19:17:13 +0200

    js

--- blog/static/calendar.js
+++ blog/static/calendar.js
@@ -2,18 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const days = document.querySelectorAll("#days");
     const dropdown = document.getElementById('wholeDrop');
     const currentDay = document.getElementById('currentDay');
+    const user = document.getElementById('user');
     days.forEach((elem, count) => {
         elem.addEventListener("click", function (ele) {
             const html = ele.target.innerHTML;
 
             const day = this.firstChild.innerHTML
             const monthYear = this.parentElement.parentElement.children[0].children[0].innerHTML
-            if (parseInt(day) >= currentDay.value){
-                dropdown.style.display = 'block'
-                dropdown.children[1].children[0].children[0].children[0].children[0].value = `${day} ${monthYear} 1`;
-                dropdown.children[1].children[1].children[0].children[0].children[0].value = `${day} ${monthYear} 0`;
-                dropdown.children[1].children[2].children[0].children[0].children[0].value = `${day} ${monthYear} 2`;
 
+            // console.log(dropdown.children[1].children[0].children[0].children[0].children[0].value)
+            if (parseInt(day) >= currentDay.value) {
+                dropdown.style.display = 'block'
+                if (user.value === "{'parent.is_parent'}"){
+                    console.log(dropdown.children[1].children[0].children[0].children[0].value = `${day} ${monthYear} 2`)
+
+                }else if (user.value === "{'teacher.is_teacher'}"){
+                    // do zrobienia
+                    console.log(user.value)
+                    console.log(dropdown.children[1].children[0].children[0].children[0].value = `${day} ${monthYear} 2`)
+
+                }else{
+                    dropdown.children[1].children[0].children[0].children[0].children[0].value = `${day} ${monthYear} 1`;
+                    dropdown.children[1].children[1].children[0].children[0].children[0].value = `${day} ${monthYear} 0`;
+                    dropdown.children[1].children[2].children[0].children[0].children[0].value = `${day} ${monthYear} 2`;
+                }
 
                 this.appendChild(dropdown)
             }
--- children/static/carousel.js
+++ children/static/carousel.js
@@ -1,15 +1,22 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+addEventListener('DOMContentLoaded', function () {
+    let items = document.querySelectorAll('.carousel .carousel-item')
+    const amount = document.getElementById('amount')
 
-		items.forEach((el) => {
-			const minPerSlide = 4
-			let next = el.nextElementSibling
-			for (let i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
+    items.forEach((el) => {
+        let minPerSlide = parseInt(amount.value)
+        if (parseInt(amount.value) <= 4) {
+            minPerSlide = 4
         }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
+
+        let next = el.nextElementSibling
+        for (let i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
 })
