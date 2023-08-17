a9d2230a10383db902bebbc7697dd6327628eb45
DawidSzoka1 <oficer130@gmail.com>
2023-08-17 13:37:03 +0200

    groups app

--- children/forms.py
+++ children/forms.py
@@ -49,20 +49,3 @@ class PaymentPlanForm(forms.ModelForm):
         }
 
 
-class GroupsForm(forms.ModelForm):
-    class Meta:
-        model = Groups
-        fields = '__all__'
-        widgets = {
-            'principal': forms.HiddenInput,
-        }
-        labels = {
-            'name': 'Nazwa:',
-            'capacity': 'Pojemność:'
-        }
-
-    def __init__(self, *args, current_user=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        if current_user is not None:
-            self.fields['photo'] = forms.ModelChoiceField(
-                queryset=Director.objects.get(user=current_user.id).groupphotos_set.filter(is_active=True))
