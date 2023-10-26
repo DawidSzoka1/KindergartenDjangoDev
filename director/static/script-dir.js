a7c243a779f0ed9f571013fa6cb8ed40879e44d3
DawidSzoka1 <oficer130@gmail.com>
2023-10-26 21:27:19 +0200

    js for search bar in give permission

--- director/static/script-dir.js
+++ director/static/script-dir.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelector("#search-button");
 
     button.addEventListener("click", function () {
+
         teachers.forEach(elem => {
             if (search.value) {
                 if (elem.children[0].children[1].children[0].innerHTML.includes(search.value) == 0) {
