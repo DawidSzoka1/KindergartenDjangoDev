d7a7d1b0accb1faba0e0bd7f993bd2c490381f9d
DawidSzoka1 <oficer130@gmail.com>
2025-07-08 23:54:39 +0200

    error handling

--- MarchewkaDjango/local_settings.py
+++ MarchewkaDjango/local_settings.py
@@ -1,16 +1,16 @@
-import os
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-SECRET = os.environ['SECRET_KEY']
-
-EMAIL_HOST_USER_SECRET = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD_SECRET = os.environ['EMAIL_HOST_PASSWORD']
+# import os
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ['DB_NAME'],
+#         'USER': os.environ['DB_USER'],
+#         'PASSWORD': os.environ['DB_PASSWORD'],
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+# SECRET = os.environ['SECRET_KEY']
+#
+# EMAIL_HOST_USER_SECRET = os.environ['EMAIL_HOST_USER']
+# EMAIL_HOST_PASSWORD_SECRET = os.environ['EMAIL_HOST_PASSWORD']
